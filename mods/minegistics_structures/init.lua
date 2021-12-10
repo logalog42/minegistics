1b66cd1d19a003df2cb1261f85e20d9710cf954c
logalog42 <logalog42@gmail.com>
2021-12-10 10:24:34 -0700

    Collector gets resources from local nodes

--- mods/minegistics_structures/init.lua
+++ mods/minegistics_structures/init.lua
@@ -3,6 +3,97 @@ minetest.register_node("minegistics_structures:Collector", {
    tiles = {"minegistics_structures_collector.png"},
 })
 
+minetest.register_abm({
+    nodenames = {"minegistics_structures:Collector"},
+    neighbors = {"basenodes:stone_with_coal"},
+    interval = 1, -- Run every 10 seconds
+    chance = 1, -- Select every 1 in 50 nodes
+    action = function(pos, node, active_object_count, active_object_count_wider)
+      local meta = minetest.get_meta({ x = pos.x, y = pos.y, z = pos.z })
+      meta:set_int("coal", (meta:get_int("coal") + 1))
+    end
+})
+
+minetest.register_abm({
+    nodenames = {"minegistics_structures:Collector"},
+    neighbors = {"basenodes:stone_with_iron"},
+    interval = 1, -- Run every 10 seconds
+    chance = 1, -- Select every nodes
+    action = function(pos, node, active_object_count, active_object_count_wider)
+      local meta = minetest.get_meta({ x = pos.x, y = pos.y, z = pos.z })
+      meta:set_int("iron", (meta:get_int("iron") + 1))
+    end
+})
+
+minetest.register_abm({
+    nodenames = {"minegistics_structures:Collector"},
+    neighbors = {"basenodes:stone_with_copper"},
+    interval = 1, -- Run every 10 seconds
+    chance = 1, -- Select every 1 in 50 nodes
+    action = function(pos, node, active_object_count, active_object_count_wider)
+      local meta = minetest.get_meta({ x = pos.x, y = pos.y, z = pos.z })
+      meta:set_int("copper", (meta:get_int("copper") + 1))
+    end
+})
+
+minetest.register_abm({
+    nodenames = {"minegistics_structures:Collector"},
+    neighbors = {"basenodes:stone_with_tin"},
+    interval = 1, -- Run every 10 seconds
+    chance = 1, -- Select every 1 in 50 nodes
+    action = function(pos, node, active_object_count, active_object_count_wider)
+      local meta = minetest.get_meta({ x = pos.x, y = pos.y, z = pos.z })
+      meta:set_int("tin", (meta:get_int("tin") + 1))
+    end
+})
+
+minetest.register_abm({
+    nodenames = {"minegistics_structures:Collector"},
+    neighbors = {"basenodes:stone_with_gold"},
+    interval = 1, -- Run every 10 seconds
+    chance = 1, -- Select every 1 in 50 nodes
+    action = function(pos, node, active_object_count, active_object_count_wider)
+      local meta = minetest.get_meta({ x = pos.x, y = pos.y, z = pos.z })
+      meta:set_int("gold", (meta:get_int("gold") + 1))
+    end
+})
+
+
+minetest.register_abm({
+    nodenames = {"minegistics_structures:Collector"},
+    neighbors = {"basenodes:cobblestone"},
+    interval = 1, -- Run every 10 seconds
+    chance = 1, -- Select every 1 in 50 nodes
+    action = function(pos, node, active_object_count, active_object_count_wider)
+      local meta = minetest.get_meta({ x = pos.x, y = pos.y, z = pos.z })
+      meta:set_int("stone", (meta:get_int("stone") + 1))
+    end
+})
+
+minetest.register_abm({
+    nodenames = {"minegistics_structures:Collector"},
+    neighbors = {"basenodes:gravel"},
+    interval = 1, -- Run every 10 seconds
+    chance = 1, -- Select every 1 in 50 nodes
+    action = function(pos, node, active_object_count, active_object_count_wider)
+      local meta = minetest.get_meta({ x = pos.x, y = pos.y, z = pos.z })
+      meta:set_int("gravel", (meta:get_int("gravel") + 1))
+    end
+})
+
+minetest.register_abm({
+    nodenames = {"minegistics_structures:Collector"},
+    neighbors = {"basenode:sand"},
+    interval = 1, -- Run every 10 seconds
+    chance = 1, -- Select every 1 in 50 nodes
+    action = function(pos, node, active_object_count, active_object_count_wider)
+      local meta = minetest.get_meta({ x = pos.x, y = pos.y, z = pos.z })
+      meta:set_int("sand", (meta:get_int("sand") + 1))
+    end
+})
+
+
+
 minetest.register_node("minegistics_structures:Factory", {
    description = " Take resources output goods",
    tiles = {"minegistics_structures_factory.png"},
