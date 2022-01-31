e6fb68c7dd9d87c1ad56c6bc6c4cd2723195c5ac
logalog42 <logalog42@gmail.com>
2022-01-31 12:39:21 -0700

    Created forest generation and made world 1 layer.

--- mods/minegistics/src/mapgen.lua
+++ mods/minegistics/src/mapgen.lua
@@ -14,46 +14,42 @@ local ores = {
 }
 
 minetest.register_on_generated(function(minp, maxp, blockseed)
-    if minp.y > 0 or maxp.y < 0 then return end
-    local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
-    local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
-    local data = vm:get_data()
-    for z = minp.z, maxp.z do
-        for x = minp.x, maxp.x do
-            for y = -2,-1,1 do
-                local vi = area:index(x, y, z)
-                local rand_gen = math.random(1,100)
-                if rand_gen >= 2 then
-                    local rand_node = math.random(1,100)
-                    if rand_node >= 15 then
-                        data[area:index(x, y, z)] = minetest.get_content_id("basenodes:dirt_with_grass")
-                    else
-                        data[area:index(x, y, z)] = minetest.get_content_id("basenodes:stone")
-                    end
-                else
-                    local rand_ore = math.random(1,100)
-                    if rand_ore >= 75 then
-                        local rand_node = math.random(1,5)
-                        data[area:index(x, y, z)] = minetest.get_content_id(ores[rand_node])
-                    else
-                        local rand_node = math.random(1,100)
-                        if rand_node >= 15 then
-                            data[area:index(x, y, z)] = minetest.get_content_id("basenodes:dirt_with_grass")
-                        else
-                            data[area:index(x, y, z)] = minetest.get_content_id("basenodes:stone")
-                        end
-                    end
-                end
+if minp.y > 0 or maxp.y < 0 then return end
+   local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+   local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
+   local data = vm:get_data()
+   for z = minp.z, maxp.z do
+      for x = minp.x, maxp.x do
+         for y = -1,-1,1 do
+            local vi = area:index(x, y, z)
+            local rand_gen = math.random(1,100)
+            if rand_gen >= 2 then
+               local rand_node = math.random(1,100)
+               if rand_node >= 15 then
+                  data[area:index(x, y, z)] = minetest.get_content_id("basenodes:dirt_with_grass")
+               else
+                  data[area:index(x, y, z)] = minetest.get_content_id("basenodes:stone")
+               end
+            else
+               local rand_ore = math.random(1,100)
+               if rand_ore >= 75 then
+                  local rand_node = math.random(1,5)
+                  data[area:index(x, y, z)] = minetest.get_content_id(ores[rand_node])
+               else
+                  local rand_node = math.random(1,100)
+                  if rand_node >= 15 then
+                     data[area:index(x, y, z)] = minetest.get_content_id("basenodes:dirt_with_grass")
+                  else
+                     data[area:index(x, y, z)] = minetest.get_content_id("basenodes:dirt_with_grass")
+                     data[area:index(x, y+1, z)] = minetest.get_content_id("basenodes:trunk")
+                     data[area:index(x, y+2, z)] = minetest.get_content_id("basenodes:leaves")
+                  end
+               end
             end
-            for y = -1,-1,1 do
-               local vi = area:index(x, y, z)
-               local rand_gen = math.random(1,100)
-               if rand_gen >= 90 and data[area:index(x,y-1,z)] == minetest.get_content_id("basenodes:dirth_with_grass") then
-                  data[area:index(x, y, z)] = minetest.get_content_id("basenodes:trunk")
-                  data[area:index(x, y+1, z)] = minetest.get_content_id("basenodes:leaves")
-            end
-        end
-    end
-    vm:set_data(data)
-    vm:write_to_map(data)
-end)
+         end
+      end
+   end
+vm:set_data(data)
+vm:write_to_map(data)
+end
+)
