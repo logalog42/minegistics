5baf6bbb789e655e0f492d4aa872115d7300a5ba
Logan Godfrey <logalog42@gmail.com>
2023-03-03 13:01:42 -0700

    New World Gen

--- mods/basenodes/init.lua
+++ mods/basenodes/init.lua
@@ -51,13 +51,37 @@ minetest.register_node("basenodes:dirt", {
 	tiles ={"default_dirt.png"},
 })
 
-minetest.register_node("basenodes:tree", {
+minetest.register_node("basenodes:tree_1", {
    descriptions = "Woods",
    drawtype="mesh",
    mesh = "minegistics_tree.obj",
-   tiles={"minegistics_tree.png"}
+   tiles={"minegistics_tree.png"},
+   sunlight_propagates = true,
 })
 
+minetest.register_node("basenodes:tree_2", {
+	descriptions = "Woods",
+	drawtype="mesh",
+	mesh = "minegistics_tree_2.obj",
+	tiles={"minegistics_tree.png"},
+	sunlight_propagates = true,
+ })
+
+minetest.register_node("basenodes:tree_3", {
+	descriptions = "Woods",
+	drawtype="mesh",
+	mesh = "minegistics_tree_3.obj",
+	tiles={"minegistics_tree.png"},
+	sunlight_propagates = true,
+})
+
+minetest.register_node("basenodes:tree_4", {
+	descriptions = "Woods",
+	drawtype="mesh",
+	mesh = "minegistics_tree_4.obj",
+	tiles={"minegistics_tree.png"},
+	sunlight_propagates = true,
+})
 minetest.register_node("basenodes:stone_with_coal", {
 	description = ("Coal Ore"),
 	tiles = {"default_stone.png^default_mineral_coal.png"},
@@ -85,12 +109,14 @@ minetest.register_node("basenodes:stone_with_gold", {
 
 minetest.register_node("basenodes:snow", {
 	description = ("Snowy mountain tops"),
-	tiles = {"default_snow.png"}
+	tiles = {"default_snow.png"},
+	paramtype = "light"
 })
 
 minetest.register_node("basenodes:snow_transition", {
 	description = ("Snow starting to show"),
-	tiles = {"default_snow.png", "default_stone.png", "default_stone.png^default_snow_side.png"}
+	tiles = {"default_snow.png", "default_stone.png", "default_stone.png^default_snow_side.png"},
+	paramtype = "light"
 })
 
 minetest.register_node("basenodes:stone_slope", {
@@ -249,7 +275,67 @@ minetest.register_node("basenodes:snow_slope", {
 	 connects_to = {"basenodes:snow", "basenodes:snow_transition", "basenodes:stone"},
  
  })
---
+
+ minetest.register_node("basenodes:sand", {
+	description = ("Desert Sand"),
+	tiles = {"default_sand.png"}
+ })
+
+ minetest.register_node("basenodes:dry_grass", {
+	description = ("Where the desert starts to take over"),
+	tiles = {"default_dry_grass.png"}
+ })
+
+ minetest.register_node("basenodes:dry_dirt", {
+	description = ("It's to warm for grass"),
+	tiles = {"default_dry_dirt.png"}
+ })
+ 
+ minetest.register_node("basenodes:sandstone", {
+	description = ("The bottom of the world"),
+	tiles = {"default_desert_sandstone.png"}
+ })
+
+ minetest.register_node("basenodes:water_source", {
+	description = ("Water Source"),
+	drawtype = "liquid",
+	waving = 3,
+	tiles = {
+		{
+			name = "default_water_source_animated.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
+		{
+			name = "default_water_source_animated.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 1,
+	liquidtype = "source",
+	liquid_alternative_source = "basenodes:water_source",
+	liquid_viscosity = 1,
+	post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+})
 -- Ore definitions
 --
 
--- mods/mapgen/init.lua
+++ mods/mapgen/init.lua
@@ -17,97 +17,119 @@ minetest.register_on_generated(function(minp, maxp, blockseed)
 
   local np_mountains = {
     offset = 0,
-     scale = 3,
-     spread = {x = 9, y = 9, z = 9},
-     seed = 800515320,
-     octaves = 10,
-     persistence = 0.1,
-     lacunarity = 1.0,
-     flags = "defaults",
+    scale = 3,
+    spread = {x = 10, y = 10, z = 10},
+    seed = 800515320,
+    octaves = 10,
+    persistence = 0.1,
+    lacunarity = 1.0,
+    flags = "defaults"
   }
 
-    local mountain_perlin = minetest.get_perlin(np_mountains)
+  local np_land = {
+    offset = 50,
+    scale = 50,
+    spread = {x = 95, y = 95, z = 95},
+    seed = 5349,
+    octaves = 3,
+    persistence = .5,
+    lacunarity = 2,
+    flags = "defaults"
+  }
 
-    local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
-    local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
-    local data = vm:get_data()
+  local mountain_perlin = minetest.get_perlin(np_mountains)
+  local land_perlin = minetest.get_perlin(np_land)
 
-    if minp.y >= -1 or maxp.y <= 1 then
-      return
-    else
-      for loopz = minp.z, maxp.z do
-        for loopx = minp.x, maxp.x do
-          for loopy = -1, -1, 1 do
+  local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+  local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
+  local data = vm:get_data()
+
+  if minp.y >= -1 or maxp.y <= 1 then
+    return
+  else
+    for loopz = minp.z, maxp.z do
+      for loopx = minp.x, maxp.x do
+        for loopy = -1, -1, 1 do
           local vi = area:index(loopx, loopy, loopz)
           local pos = { x = loopx, y = loopy, z = loopz}
-          local current_perlin = math.floor(mountain_perlin:get_3d(pos))
+          local height_perlin = mountain_perlin:get_3d(pos)
+          local height_floor_perlin = math.floor(mountain_perlin:get_3d(pos))
+          local land_perlin = math.floor(land_perlin:get_3d(pos))
 
-          --new perlin generating
-          --Mountain locations
-          if  current_perlin > 1 then
+          if land_perlin < 51 then --Land and Sea generating
+            --Mountain locations
+            if  height_perlin >= 1 then
 
-            --adjacent nodes
-            local right_node = {x = loopx + 1, y = loopy, z = loopz}
-            local left_node = {x = loopx - 1, y = loopy, z = loopz}
-            local back_node = {x = loopx, y = loopy, z = loopz + 1}
-            local front_node = {x = loopx, y = loopy, z = loopz - 1}
-            local adjacent_node = {right_node, left_node, back_node, front_node}
-            local sides = 0
+              --adjacent nodes
+              local right_node =       {x = loopx + 1, y = loopy, z = loopz}
+              local left_node =        {x = loopx - 1, y = loopy, z = loopz}
+              local back_node =        {x = loopx,     y = loopy, z = loopz + 1}
+              local front_node =       {x = loopx,     y = loopy, z = loopz - 1}
+              --[[
+              local right_front_node = {x = loopx + 1, y = loopy, z = loopz - 1}
+              local right_back_node =  {x = loopx + 1, y = loopy, z = loopz + 1}
+              local left_front_node =  {x = loopx - 1, y = loopy, z = loopz - 1}
+              local left_back_node =   {x = loopx - 1, y = loopy, z = loopz + 1}
+              ]]--
 
-            --Placing down mountains
+              local adjacent_node = {right_node, left_node, back_node, front_node}
 
-            for level = loopy, math.floor(current_perlin) do
-              if level >= 2 then
-                data[area:index(loopx, level, loopz)] = minetest.get_content_id("basenodes:snow")
-              elseif level == 1 then
-                data[area:index(loopx, level, loopz)] = minetest.get_content_id("basenodes:snow_transition")
+              --Placing down mountains
+              for level = loopy, math.floor(height_perlin) do
+                if level >= 2 then
+                  data[area:index(loopx, level, loopz)] = minetest.get_content_id("basenodes:snow")
+                elseif level == 1 then
+                  data[area:index(loopx, level, loopz)] = minetest.get_content_id("basenodes:snow_transition")
+                else
+                  data[area:index(loopx, level, loopz)] = minetest.get_content_id("basenodes:stone")
+                end
+              end
+
+              for i, value in ipairs(adjacent_node) do
+                if math.floor(mountain_perlin:get_3d(value)) > height_floor_perlin then
+                  data[area:index(loopx, height_floor_perlin + 1, loopz)] = minetest.get_content_id("basenodes:snow_slope")
+                  data[area:index(loopx, height_floor_perlin + 2, loopz)] = minetest.get_content_id("air")
+                elseif math.floor(mountain_perlin:get_3d(value)) < 1 then
+                  data[area:index(loopx, height_floor_perlin, loopz)] = minetest.get_content_id("basenodes:snow_slope_transition")
+                  data[area:index(loopx, height_floor_perlin + 1, loopz)] = minetest.get_content_id("air")
+                end
+              end
+
+              --Edge around mountain for ore generation
+            elseif height_perlin >= .7 and height_perlin < 1 then
+              local rand_ore = math.random(1,200)
+              if rand_ore >= 199 then
+                local rand_node = math.random(1, 5)
+                data[area:index(loopx, loopy, loopz)] = minetest.get_content_id(ores[rand_node])
+              elseif rand_ore >= 198 then
+                local rand_node = math.random(1, 4)
+                data[area:index(loopx, loopy, loopz)] = minetest.get_content_id(ores[rand_node])
               else
-                data[area:index(loopx, level, loopz)] = minetest.get_content_id("basenodes:stone")
+                data[area:index(loopx, loopy, loopz)] = minetest.get_content_id("basenodes:stone")
               end
-            end
 
-            --slope node selection
-            for _, adjacentNode in ipairs(adjacent_node) do
-              if math.floor(mountain_perlin:get_3d(adjacentNode)) > current_perlin then
-                sides = sides + 1
-              end
-            end
-
-            --Place slope node
-            if sides >= 1 then
-              if (current_perlin + 1) >= 3 then
-              data[area:index(loopx, current_perlin + 1, loopz)] = minetest.get_content_id("basenodes:snow_slope")
-              elseif (current_perlin + 1) == 2 then
-                data[area:index(loopx, current_perlin + 1, loopz)] = minetest.get_content_id("basenodes:snow_slope_transition")
-              else
-                data[area:index(loopx, current_perlin + 1, loopz)] = minetest.get_content_id("basenodes:stone_slope")
-              end
-            end
-
-            sides = 0
-
-          --Edge around mountain for ore generation
-          elseif current_perlin <= 1 and current_perlin >= 0 then
-            local rand_ore = math.random(1,100)
-            if rand_ore >= 99 then
-              local rand_node = math.random(1, 5)
-              data[area:index(loopx, loopy, loopz)] = minetest.get_content_id(ores[rand_node])
-            elseif rand_ore >= 98 then
-              local rand_node = math.random(1, 4)
-              data[area:index(loopx, loopy, loopz)] = minetest.get_content_id(ores[rand_node])
+            elseif height_perlin < -2 then
+              data[area:index(loopx, loopy, loopz)] = minetest.get_content_id("basenodes:dirt_with_grass")
+              data[area:index(loopx, loopy + 1, loopz)] = minetest.get_content_id("basenodes:tree_" .. math.random(1,4))
+              
+            --Everything else is grass
             else
-              data[area:index(loopx, loopy, loopz)] = minetest.get_content_id("basenodes:stone")
+                data[area:index(loopx, loopy, loopz)] = minetest.get_content_id("basenodes:dirt_with_grass")
             end
 
-          --Everything else is grass
-          else
-            data[area:index(loopx, loopy, loopz)] = minetest.get_content_id("basenodes:dirt_with_grass")
+          elseif land_perlin < 54 and land_perlin >= 51 then -- Border
+            data[area:index(loopx, loopy, loopz)] = minetest.get_content_id("basenodes:sand")
+          else --Generating Ocean
+            data[area:index(loopx, loopy, loopz)] = minetest.get_content_id("basenodes:water_source")
           end
-       end
-     end
-   end
- end
-vm:set_data(data)
-vm:write_to_map(data)
-end
-)
+          for loopy = -2, -2, 1 do
+            data[area:index(loopx, loopy, loopz)] = minetest.get_content_id("basenodes:sandstone")
+          end
+        end
+      end
+    end
+  end
+  
+  vm:set_data(data)
+  vm:write_to_map(data)
+end)
